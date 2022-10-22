73869cbadf992e312bcb247fd1bb9f624e05dcf0
siyi424 <siyihuang_sk8@163.com>
2022-10-22 20:23:50 +0800

    add AddInfo app and its models

--- mytSite/addInfo/admin.py
+++ mytSite/addInfo/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from addInfo.models import PassedPerson
+
 # Register your models here.
+admin.site.register(PassedPerson)
--- mytSite/addInfo/models.py
+++ mytSite/addInfo/models.py
@@ -1,3 +1,13 @@
+from tabnanny import verbose
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+class PassedPerson(models.Model):
+    name = models.CharField(max_length=30, verbose_name='逝者名字')
+    birth = models.CharField(max_length=100, verbose_name='出生日期')
+    passDate = models.CharField(max_length=100, verbose_name='逝世日期')
+    gender = models.CharField(max_length=10, verbose_name='性别')
+    desc = models.TextField(max_length=300, verbose_name='介绍文字')
+
+    
